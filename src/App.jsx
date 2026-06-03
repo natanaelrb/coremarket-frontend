1c52322ff20191706108e7c2b7b4d4bfe61842d5
Natanael Ribeiro <ribeironatanael155@gmail.com>
2026-06-03 10:01:46 -0300

    feat: configurar React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,11 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import Layout from "./layouts/Layout";
-
-import Dashboard from "./pages/Dashboard";
-import Clientes from "./pages/Clientes";
-import Fiado from "./pages/Fiado";
-import Estoque from "./pages/Estoque";
-import Relatorios from "./pages/Relatorios";
+import AppRoutes from "./routes/AppRoutes";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/clientes" element={<Clientes />} />
-          <Route path="/fiado" element={<Fiado />} />
-          <Route path="/estoque" element={<Estoque />} />
-          <Route path="/relatorios" element={<Relatorios />} />
-        </Routes>
-      </Layout>
-    </BrowserRouter>
+    <Layout>
+      <AppRoutes />
+    </Layout>
   );
 }
 
