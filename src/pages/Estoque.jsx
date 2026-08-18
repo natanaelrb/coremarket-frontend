a45a7c9fd59dbe94bcdc2a8e0f031caff49094a6
Natanael Ribeiro <ribeironatanael155@gmail.com>
2026-08-18 18:57:18 -0300

    refactor: remover componentes e serviços legados

--- src/pages/Estoque.jsx
+++
@@ -1,7 +0,0 @@
-export default function Estoque() {
-  return (
-    <div>
-      <h1 className="text-3xl font-bold">Estoque</h1>
-    </div>
-  );
-}
--- src/pages/Fiado.jsx
+++
@@ -1,3 +0,0 @@
-export default function Fiado() {
-  return <h1 className="text-3xl font-bold text-gray-800">Fiado</h1>;
-}
